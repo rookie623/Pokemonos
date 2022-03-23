823b341904f58423fe709b7fd6f67862aaaf9986
Matias Menoni <rookiemat@gmail.com>
2022-03-23 15:43:05 -0300

    Agregado 3 colores, refactor rederizado de pokemones

--- statics/script.js
+++ statics/script.js
@@ -2,36 +2,73 @@ const contenedor = document.querySelector(".container");
 
 let pokemons;
 async function traerInfo() {
-  const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150&offset=0");
+  const respuesta = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=88&offset=0"
+  );
   const data = await respuesta.json();
-  console.log(data);
+  //console.log(data);
   data.results.forEach(async (pokemon) => {
-      const respPoke = await fetch(`${pokemon.url}`)
-      const dataPoke = await respPoke.json()
-      console.log(dataPoke);
-    contenedor.innerHTML += `<div class="card">
-    <div class="card__thumnail">
-        <div class="card__types">
-            <h6 class="card_type">${dataPoke.types[0].type.name}</h6>
-        </div>
-        <img class="card__img" src="${dataPoke.sprites.front_default}" alt="">
-        <div class="card__stats">
-            <h3 class="card__name">${pokemon.name}</h3>
-            <ul class="card__stats-list">
-                <li class="card__stat">ID : <span class="card__stat-points">${dataPoke.id}</span></li>
-                <li class="card__stat">Power : <span class="card__stat-points">${dataPoke.stats[5].base_stat}</span></li>
-                <li class="card__stat">Damage : <span class="card__stat-points">${dataPoke.stats[2].base_stat}</span></li>
-                <li class="card__stat">Ability : <span class="card__stat-points">${dataPoke.abilities[0].ability.name}</span></li>
-                <li class="card__stat">Attacks : <span class="card__stat-points">${dataPoke.stats[1].base_stat}</span></li>
-                <li class="card__stat">Health : <span class="card__stat-points">${dataPoke.stats[0].base_stat}</span></li>
-                <li class="card__stat">Friendly : <span class="card__stat-points">${dataPoke.stats[4].base_stat}</span></li>
-            </ul>
-            <h5 class="card__footer">Pokemon Cards</h5>
-        </div>
-    </div>
-</div>`;
+    const respPoke = await fetch(`${pokemon.url}`);
+    const dataPoke = await respPoke.json();
+
+    //   RENDERIZADO DE POKEMONES
+    const card = document.createElement("div");
+    card.className = "card";
+    const cardThumbnail = document.createElement("div");
+    cardThumbnail.className = "card__thumbnail";
+    const cardTypes = document.createElement("div");
+    cardTypes.className = "card__types";
+    const type = document.createElement("h6");
+    type.className = "card_type";
+    type.innerText = `${dataPoke.types[0].type.name}`;
+    const image = document.createElement("img");
+    image.classList.add("card__img");
+    image.src = `${dataPoke.sprites.front_default}`;
+    const cardStats = document.createElement("div");
+    cardStats.className = "card__stats";
+    const cardName = document.createElement("h3");
+    cardName.classList.add("card__name");
+    cardName.innerText = `${pokemon.name}`;
+    cardStats.appendChild(cardName);
+    const statsList = document.createElement("ul");
+    statsList.className = "card__stats-list";
+    let stat = `<li class="card__stat">ID : <span class="card__stat-points">${dataPoke.id}</span></li>`;
+    stat += `<li class="card__stat">Power : <span class="card__stat-points">${dataPoke.stats[5].base_stat}</span></li>`
+    stat += `<li class="card__stat">Damage : <span class="card__stat-points">${dataPoke.stats[2].base_stat}</span></li>`
+    stat += `<li class="card__stat">Ability : <span class="card__stat-points">${dataPoke.abilities[0].ability.name}</span></li>`
+    stat += `<li class="card__stat">Attacks : <span class="card__stat-points">${dataPoke.stats[1].base_stat}</span></li>`
+    stat += `<li class="card__stat">Health : <span class="card__stat-points">${dataPoke.stats[0].base_stat}</span></li>`
+    stat += `<li class="card__stat">Friendly : <span class="card__stat-points">${dataPoke.stats[4].base_stat}</span></li>`
+    statsList.innerHTML = stat;
+    cardStats.appendChild(statsList);
+    const footerCard = document.createElement("h5");
+    footerCard.className = "card__footer";
+    footerCard.innerText = "Pokemon Cards";
+
+    if (dataPoke.types[0].type.name == "grass") {
+      card.classList.add("card__b--green");
+      cardThumbnail.classList.add("card__t--green");
+      cardStats.classList.add("card__s--green");
+    }
+    if (dataPoke.types[0].type.name == "fire") {
+      card.classList.add("card__b--red");
+      cardThumbnail.classList.add("card__t--red");
+      cardStats.classList.add("card__s--red");
+    }
+    if (dataPoke.types[0].type.name == "water") {
+      card.classList.add("card__b--blue");
+      cardThumbnail.classList.add("card__t--blue");
+      cardStats.classList.add("card__s--blue");
+    }
+
+    cardTypes.appendChild(type);
+    cardThumbnail.appendChild(cardTypes);
+    cardStats.appendChild(footerCard);
+    cardThumbnail.appendChild(image);
+    cardThumbnail.appendChild(cardStats);
+    card.appendChild(cardThumbnail);
+    contenedor.appendChild(card);
   });
 }
 
 traerInfo();
-
